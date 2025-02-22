0bca026b47c225b7ab7ce9a19e510ed705fe8326
aki <arisu@ieee.org>
2025-02-22 13:39:13 +0800

    feat：have to hello offical aimedb

--- server/aimedb/handlers/felica.go
+++ server/aimedb/handlers/felica.go
@@ -1,10 +1,31 @@
 package handlers
 
-import "OMG_ITS_ALLNET_SERVER/server/aimedb"
+import (
+	"OMG_ITS_ALLNET_SERVER/server/aimedb"
+	"OMG_ITS_ALLNET_SERVER/utils"
+)
 
 type FeliCaHandler struct{}
 
 func (h *FeliCaHandler) Handle(header *aimedb.AimeDbHeader, payload []byte) (uint16, []byte, error) {
-	// TODO: Implement
-	return 0x0000, []byte{0x01, 0x23, 0x45, 0x67}, nil // Example success response
+	switch utils.HexToDec(header.CommandID) {
+	case 1:
+		if len(payload) < 16 {
+			return 0, nil, nil
+		} else {
+			idm := payload[:8]
+			pmm := payload[8:]
+			return 0, GetFelicaID(pmm, idm), nil
+		}
+	case 2:
+		break
+	case 3:
+		break
+	}
+
+	return 0, nil, nil
+}
+
+func GetFelicaID(pmm []byte, idm []byte) []byte {
+	return nil
 }
--- utils/numbers.go
+++ utils/numbers.go
@@ -3,3 +3,7 @@ package utils
 func DecToHex(dec int) uint16 {
 	return uint16(dec)
 }
+
+func HexToDec(hex uint16) int {
+	return int(hex)
+}
